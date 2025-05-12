3a25f4782d78d65f641f77a4be841160a63fe88e
Tedros <tbeya1@unh.newhaven.edu>
2025-05-12 12:23:08 -0400

    More updates

--- server.js
+++ server.js
@@ -7,16 +7,22 @@ const app = express();
 const PORT = process.env.PORT || 9999;
 
 // Middleware
-app.use(cors());
+const cors = require('cors');
+app.use(cors({ origin: 'https://worldtimeunh.netlify.app' }));
+
+//app.use(cors());
 app.use(express.json());
 
-// MongoDB connection URI
 const mongoURI = 'mongodb+srv://tbeya1:1Poilkjmnb%40@cluster0.v5tl8ke.mongodb.net/timeinfo?retryWrites=true&w=majority&appName=Cluster0';
 
 // Connect to MongoDB
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('MongoDB connection error:', err));
+
 
 // Schema
 const TimeSchema = new mongoose.Schema({
